66dd7c97512eecc66bac3ede0ecdc782441a3ed8
lyman wong <lymanwong@gmail.com>
2017-06-20 14:56:39 -0700

    added new xml request

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,25 @@
 (function ($) {
   //Modal tool tip hide
-  function createCORSRequest(method, url){
-      var xhr = new XMLHttpRequest();
-      if ("withCredentials" in xhr){
-          xhr.open(method, url, true);
-      } else if (typeof XDomainRequest != "undefined"){
-          xhr = new XDomainRequest();
-          xhr.open(method, url);
-      } else {
-          xhr = null;
-      }
-      return xhr;
-  }
+  function createCORSRequest(method, url) {
+     var xhr = new XMLHttpRequest();
+     if ("withCredentials" in xhr) {
+        // Check if the XMLHttpRequest object has a "withCredentials" property.
+        // "withCredentials" only exists on XMLHTTPRequest2 objects.
+        xhr.open(method, url, true);
+     }
+     else if (typeof XDomainRequest != "undefined") {
+        // Otherwise, check if XDomainRequest.
+        // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
+        xhr = new XDomainRequest();
+        xhr.open(method, url);
+     }
+     else {
+        // Otherwise, CORS is not supported by the browser.
+        xhr = null;
+        alert("CORS is not supported by the browser");
+     }
+     return xhr;
+    }
 
 $('.modal-author-hint,.modal-title-hint,.modal-publisher-hint,.modal-contributor-hint').slideUp();
 
